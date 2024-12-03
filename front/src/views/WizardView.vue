28900f9cbaeca1f197868240e4e4e5fbd7696dd3
a23bBrianJaen <a23brijaemed@inspedralbes.cat>
2024-12-03 11:58:27 +0100

    feat: create component wizard and composable

---
+++ front/src/views/WizardView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useWizardView} from "@/composable/useWizardView.js";
+
+const wizardView = useWizardView()
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
