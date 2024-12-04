86b1f170dca65a022c2c02160f0fda3cc841c138
Lorenzo Moll <lorenzo.moll@precognis.com>
2024-12-04 14:53:55 +0100

    Implemented return response to sidebar and progress bar

--- front/src/composable/useWizardView.js
+++ front/src/composable/useWizardView.js
@@ -1,65 +1,100 @@
-import {onBeforeMount, reactive, ref} from 'vue';
+import { onBeforeMount, reactive, ref } from 'vue';
 import questions from '../assets/questions.json'
 
 
 export function useWizardView() {
 
-  // Sidebar Status
-  const showSidebar = ref(true);
-  const templateData = reactive({questions: questions})
+  //Data of questions
+  const templateData = reactive({ questions: questions })
+  const currentQuestionIndex = ref(0)
 
-  // Lista de estudiantes
+  // List of students
   const students = ref([
-    {id: 1, name: 'Juan Pérez', image: 'https://via.placeholder.com/50'},
-    {id: 2, name: 'María López', image: 'https://via.placeholder.com/50'},
-    {id: 3, name: 'Carlos Ramírez', image: 'https://via.placeholder.com/50'},
-    {id: 4, name: 'Juan Pérez', image: 'https://via.placeholder.com/50'},
-    {id: 5, name: 'María López', image: 'https://via.placeholder.com/50'},
-    {id: 6, name: 'Carlos Ramírez', image: 'https://via.placeholder.com/50'},
-    {id: 7, name: 'Juan Pérez', image: 'https://via.placeholder.com/50'},
-    {id: 8, name: 'María López', image: 'https://via.placeholder.com/50'},
-    {id: 9, name: 'Carlos Ramírez', image: 'https://via.placeholder.com/50'},
-    {id: 10, name: 'Juan Pérez', image: 'https://via.placeholder.com/50'},
-    {id: 11, name: 'María López', image: 'https://via.placeholder.com/50'},
-    {id: 12, name: 'Carlos Ramírez', image: 'https://via.placeholder.com/50'},
+    { id: 1, name: 'Juan Pérez', image: 'https://via.placeholder.com/50' },
+    { id: 2, name: 'María López', image: 'https://via.placeholder.com/50' },
+    { id: 3, name: 'Carlos Ramírez', image: 'https://via.placeholder.com/50' },
+    { id: 4, name: 'Juan Pérez', image: 'https://via.placeholder.com/50' },
+    { id: 5, name: 'María López', image: 'https://via.placeholder.com/50' },
+    { id: 6, name: 'Carlos Ramírez', image: 'https://via.placeholder.com/50' },
+    { id: 7, name: 'Juan Pérez', image: 'https://via.placeholder.com/50' },
+    { id: 8, name: 'María López', image: 'https://via.placeholder.com/50' },
+    { id: 9, name: 'Carlos Ramírez', image: 'https://via.placeholder.com/50' },
+    { id: 10, name: 'Juan Pérez', image: 'https://via.placeholder.com/50' },
+    { id: 11, name: 'María López', image: 'https://via.placeholder.com/50' },
+    { id: 12, name: 'Carlos Ramírez', image: 'https://via.placeholder.com/50' },
   ]);
 
   const responses = ref([null, null, null]);
-  const draggedStudent = ref(null); // Estudiante arrastrado
+  const draggedStudent = ref(null); 
 
   //Save the student dragged
   const onDragStart = (student) => {
     draggedStudent.value = student;
   };
 
-  //Cuando sueltas el estudiante
+  //When I delete the student, I add him to the answers and remove him from the sidebar
   const onDrop = (index) => {
     if (draggedStudent.value) {
-      // Agregar al espacio de respuesta
       responses.value[index] = draggedStudent.value;
 
-      // Quitar del sidebar
       students.value = students.value.filter(
         (student) => student.id !== draggedStudent.value.id
       );
 
-      // Limpiar estudiante arrastrado
       draggedStudent.value = null;
     }
   };
 
-  onBeforeMount( () => {
+  //Function that returns the answer to the students array and removes the answers
+  const onDropReturn = () => {
+    const indexStudent = students.value.findIndex((student) => student && student.id === draggedStudent.value.id);
+    if (draggedStudent.value && indexStudent == -1) {
+      //Check if the student exists
+      const exists = students.value.some((s) => s.id === draggedStudent.id);
+      if (!exists) {
+        students.value.push(draggedStudent.value);
+
+        //Delete response
+        const indexInResponses = responses.value.findIndex((response) => response && response.id === draggedStudent.value.id);
+        if (indexInResponses !== -1) {
+          responses.value[indexInResponses] = null;
+          draggedStudent.value = null;
+        }
+      }
+    }
+  }
+
+  //Calculate the progress for the sidebar
+  const calculateProgress = () => {
+    const totalQuestions = templateData.questions.length; // Total questions
+    const currentProgress = currentQuestionIndex.value + 1; // Current question (starting from 1)
+    return (currentProgress / totalQuestions) * 100;
+  }
+
+  const nextQuestion = () => {
+
+    if (currentQuestionIndex.value < templateData.questions.length - 1) {
+      currentQuestionIndex.value++;
+    } else {
+      console.log("Ya estás en la última pregunta.");
+    }
+  }
+
+  onBeforeMount(() => {
 
     templateData.questions = questions.questions;
     console.log(templateData.questions)
   })
 
   return {
-    showSidebar,
     students,
     responses,
+    currentQuestionIndex,
     onDragStart,
     onDrop,
+    onDropReturn,
+    calculateProgress,
+    nextQuestion,
     templateData,
   };
 }
--- front/src/views/WizardView.vue
+++ front/src/views/WizardView.vue
@@ -1,25 +1,31 @@
 <script>
-import {useWizardView} from '@/composable/useWizardView.js';
+import { useWizardView } from '@/composable/useWizardView.js';
 
 export default {
 
   setup() {
     // Usamos la lógica del composable
     const {
-      showSidebar,
       students,
       responses,
+      currentQuestionIndex,
       onDragStart,
       onDrop,
+      onDropReturn,
+      calculateProgress,
+      nextQuestion,
       templateData,
     } = useWizardView();
 
     return {
-      showSidebar,
       students,
       responses,
+      currentQuestionIndex,
       onDragStart,
       onDrop,
+      onDropReturn,
+      calculateProgress,
+      nextQuestion,
       templateData,
     };
   },
@@ -29,16 +35,16 @@ export default {
 <template>
   <q-layout>
     <!-- Sidebar -->
-    <q-drawer v-model="showSidebar" side="right" :width="400" bordered>
+    <q-drawer side="right" :width="400" bordered @dragover.prevent @drop="onDropReturn()">
       <q-list>
         <q-item-label header class="text-h6 text-center q-mb-lg">
           Llista d'Estudiants
         </q-item-label>
         <div class="row">
-          <div class="col-4 text-center q-mb-lg" v-for="student in students" :key="student.id"
-               draggable="true" @dragstart="onDragStart(student)">
+          <div class="col-4 text-center q-mb-lg" v-for="student in students" :key="student.id" draggable="true"
+            @dragstart="onDragStart(student)">
             <q-avatar size="lg" class="q-mb-sm">
-              <img :src="student.image" alt="Foto de Estudiante"/>
+              <img :src="student.image" alt="Foto de Estudiante" />
             </q-avatar>
             <q-item-label>{{ student.name }}</q-item-label>
           </div>
@@ -50,32 +56,42 @@ export default {
       <q-page class="flex flex-center q-pa-lg-lg">
         <!-- Contenedor centrado -->
         <div class="questions-div q-pa-md">
-            <!-- Título centrado -->
-              <q-icon name="help_outline" size="lg" color="primary" class="q-icon">
-                <q-tooltip>
-                  {{ templateData.questions[9].description }}
-                </q-tooltip>
-              </q-icon>
+          <!-- Título centrado -->
+          <q-icon name="help_outline" size="lg" color="primary" class="q-icon">
+            <q-tooltip>
+              {{ templateData.questions[9].description }}
+            </q-tooltip>
+          </q-icon>
 
-            <h3 class="text-center">{{ templateData.questions[9].question }}
-            </h3>
-            <div class="flex flex-center items-center justify-center" >
-              <div class="row q-gutter-md">
-                <div v-for="(response, index) in responses" :key="index"
-                     class="response col-12 col-sm-4 col-md-3 text-center flex flex-center"
-                     @dragover.prevent @drop="onDrop(index)">
-                  <div class="flex flex-column items-center">
-                    <q-avatar size="lg" v-if="response" class="q-mb-sm">
-                      <img :src="response.image" alt="Respuesta"/>
-                    </q-avatar>
-                    <p v-else class="text-grey q-mb-none">RESPUESTA</p>
-                    <q-item-label v-if="response" class="q-mb-none">{{ response.name }}</q-item-label>
-                  </div>
+          <div>
+            <h4 class="text-center q-mb-md">Pregunta {{ currentQuestionIndex + 1 }}</h4>
+            <div class="w-full bg-gray-200 rounded-full h-4">
+              <div class="bg-blue-500 h-4 rounded-full" :style="{ width: calculateProgress() + '%' }"></div>
+            </div>
+          </div>
+
+          <h3 class="text-center">{{ templateData.questions[9].question }}
+          </h3>
+          <div class="flex flex-center items-center justify-center">
+            <div class="row q-gutter-md">
+              <div v-for="(response, index) in responses" :key="index"
+                class="response col-12 col-sm-4 col-md-3 text-center flex flex-center" @dragover.prevent
+                @drop="onDrop(index)">
+                <div class="flex flex-col items-center" draggable="true" @dragstart="onDragStart(response)">
+                  <q-avatar size="lg" v-if="response" class="q-mb-sm">
+                    <img :src="response.image" alt="Respuesta" />
+                  </q-avatar>
+                  <p v-else class="text-grey q-mb-none">RESPUESTA</p>
+                  <q-item-label v-if="response" class="q-mb-none">{{ response.name }}</q-item-label>
                 </div>
               </div>
             </div>
+          </div>
 
-
+          <!-- Botón para avanzar a la siguiente pregunta -->
+          <button @click="nextQuestion()" class="bg-blue-500 text-white py-2 px-4 rounded">
+            Siguiente Pregunta
+          </button>
         </div>
       </q-page>
     </q-page-container>
