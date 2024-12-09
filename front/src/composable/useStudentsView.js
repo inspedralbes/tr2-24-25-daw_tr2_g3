66dc16b2653df5dcb16ccb1df4d2de2c89909bcd
Lorenzo Moll <lorenzo.moll@precognis.com>
2024-12-09 10:02:42 +0100

    Search and dropdown initial

--- front/src/composable/useStudentsView.js
+++ front/src/composable/useStudentsView.js
@@ -9,9 +9,45 @@ export function useStudentsView() {
     { id: 5, name: 'Pedro Sánchez', image: 'https://via.placeholder.com/50', dni: '12345678A', email: 'aasd@gmail.com', phone: '123456789', address: 'Calle Falsa 123' },
   ]);
   
+  const search = ref('');
+
   const nStudents = ref(students.value.length);
 
+  const dropdownOpen = ref(false);
+
+  const options = ref([
+    {
+      id: 1,
+      name: "Nombre",
+    },
+    {
+      id: 2,
+      name: "DNI",
+    },
+    {
+      id: 3,
+      name: "Curso",
+    },
+  ]);
+
+  const selectedOption = ref(options.value[0]);
+
+  const toggleDropdown = () => {
+    dropdownOpen.value = !dropdownOpen.value;
+  };
+
+  const selectOption = (option) => {
+    selectedOption.value = option;
+    dropdownOpen.value = false;
+  };
+
   return {
+    search,
+    dropdownOpen,
+    options,
+    selectedOption,
+    toggleDropdown,
+    selectOption,
     students,
     nStudents,
   };
--- front/src/views/StudentsView.vue
+++ front/src/views/StudentsView.vue
@@ -3,21 +3,84 @@ import LayoutMain from "@/layout/LayoutMain.vue";
 import StudentsGrid from "@/components/StudentsGrid.vue";
 import { useStudentsView } from '@/composable/useStudentsView.js';
 
-const { students, nStudents } = useStudentsView();
+const { 
+  students, 
+  nStudents,
+  search,
+  dropdownOpen,
+  options,
+  selectedOption,
+  toggleDropdown,
+  selectOption,
+} = useStudentsView();
 </script>
 
 <template>
-    <LayoutMain>
+  <LayoutMain>
+    <template #title>
+      <p>Estudiants</p>
+    </template>
 
-      <template #title>
-        <p>Estudiants</p>
-      </template>
+    <template #subtitle>
+      <p>{{ nStudents }}</p>
+    </template>
+    <div class="flex items-center">
+      <!-- Search -->
+      <div class="relative flex items-stretch search-container">
+        <input
+          type="text"
+          placeholder="Buscar..."
+          v-model="search"
+          class="block px-3 py-1.5 text-base font-normal leading-6 text-gray-900 bg-white bg-clip-padding border border-gray-300 rounded-l-md focus:outline-none"
+        />
+        <span class="flex items-center px-3 py-1.5 text-base font-normal leading-6 text-current text-center whitespace-nowrap bg-gray-100 border border-gray-300 border-l-0 rounded-r-md">
+          <i class="bi bi-search"></i>
+        </span>
+      </div>
+      <!-- DropDown -->
+      <div class="relative ml-8">
+        <button
+          @click="toggleDropdown"
+          class="grid w-full cursor-pointer grid-cols-1 rounded-md bg-white py-2 px-3 text-left text-gray-900 border-2 border-gray-300 focus:outline-none"
+          aria-haspopup="listbox"
+          :aria-expanded="dropdownOpen"
+          aria-labelledby="listbox-label"
+        >
+          <span class="flex items-center gap-3">
+            <span class="block truncate">{{ selectedOption.name }}</span>
+          </span>
+        </button>
 
-      <template #subtitle>
-        <p>{{ nStudents }}</p>
-      </template>
+        <ul
+          v-if="dropdownOpen"
+          class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none"
+          tabindex="-1"
+          role="listbox"
+        >
+          <li
+            v-for="option in options"
+            :key="option.id"
+            @click="selectOption(option)"
+            class="cursor-pointer select-none py-2 px-3 text-gray-900 hover:bg-indigo-600 hover:text-white"
+          >
+            {{ option.name }}
+          </li>
+        </ul>
+      </div>
+    </div>
 
-      <StudentsGrid :students="students" :nStudents="nStudents" />
-
-    </LayoutMain>
+    <StudentsGrid :students="students" :nStudents="nStudents" />
+  </LayoutMain>
 </template>
+
+<style scoped>
+/* Delete focus blue*/
+.search-container input:focus,
+.search-container span:focus,
+button:focus,
+ul:focus {
+  outline: none;
+  box-shadow: none;
+  border-color: inherit;
+}
+</style>
