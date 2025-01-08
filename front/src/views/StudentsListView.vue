40210650229e32321f2fd1f654f977ee932ace7b
a23bBrianJaen <a23brijaemed@inspedralbes.cat>
2025-01-08 12:02:10 +0100

    fix: pagination don't work

--- front/src/views/StudentsListView.vue
+++ front/src/views/StudentsListView.vue
@@ -17,25 +17,30 @@ const studentView = useStudentsListView(props.dataProps);
 <template>
   <span class="text-2xl font-bold ">Lista de estudiantes</span>
   <div>
+    <!--  {{dataProps[0]}}-->
+    <!--  {{dataProps[0].user.name}}-->
+    <!--  {{dataProps[0].role}}-->
 
-      <div class="grid mt-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 ">
-        <div
+    <!--    {{dataProps.length}}-->
+
+    <div class="grid mt-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 ">
+      <div
           class="p-4 border rounded-lg shadow-md flex items-center max-w-xs transform transition-transform duration-200 hover:scale-105"
           v-for="student in dataProps"
           :key="student.id"
-        >
-          <img :src="student.image" :alt="student.user.name" class="w-16 h-16 rounded-full mr-6"/>
-          <div class="flex flex-col">
-            <!-- Add lastname -->
-            <h6 class="line-clamp-1 break-all w-[170px] mb-1">{{ student.user.name }} {{ student.user.lastname }}</h6>
-            <div class="line-clamp-1 break-all w-[170px]"><span class="font-bold">Email:  </span>{{ student.user.email }}</div>
-            <div class="line-clamp-1 break-all w-[170px]">
-            </div>
+      >
+        <img :src="student.image" :alt="student.user.name" class="w-16 h-16 rounded-full mr-6"/>
+        <div class="flex flex-col">
+          <!-- Add lastname -->
+          <h6 class="line-clamp-1 break-all w-[170px] mb-1">{{ student.user.name }} {{ student.user.lastname }}</h6>
+          <div class="line-clamp-1 break-all w-[170px]"><span class="font-bold">Email:  </span>{{ student.user.email }}</div>
+          <div class="line-clamp-1 break-all w-[170px]">
           </div>
         </div>
       </div>
+    </div>
   </div>
-<!--  <StudentsListGrid/>-->
+  <!--  <StudentsListGrid/>-->
   <Pagination
       :totalItems="studentView.nStudents.value"
       :currentPage="studentView.currentPage.value"
